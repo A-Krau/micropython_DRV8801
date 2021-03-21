d2ece8cda68fbc627983d3367c93dfbb4f1dd6e5
A-Krau <aric.j.krause@gmail.com>
2021-03-21 12:37:44 -0700

    Update main.py
    Updated to reflect changes made to pololu.py.

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-from machine import Pin, PWM
+from machine import Pin
+from machine import PWM
 from time import sleep
 from pololu import Motor_Driver
 
-duty =65000
+duty = 15000
 freq = 1000
 
-motor = Motor_Driver(16,17,None,freq,duty,26,27)
-            #(self, dir_pin, speed_pin, bk_pin, freq, duty, motor_sense, current_sense)
+motor = Motor_Driver(16,17,None,None,26,27)
+            #(self, dir_pin, speed_pin, bk_pin, sleep_pin, motor_sense, current_sense):
 
 while True:
-    motor.speed()
+    motor.speed(freq, duty)
     if motor.pot_read() <= 400:
-        motor.direction(1)
+        motor.extend()
     if motor.pot_read() >= 65000:
-        motor.direction(0)
+        motor.retract()
     print(motor.pot_read())
     motor.current_read()
     sleep(.1)
