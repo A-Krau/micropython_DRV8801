2f76afdff640f5697b9d9d2665291b41cef6a86e
A-Krau <aric.j.krause@gmail.com>
2021-03-21 18:01:59 -0700

    Used for testing pololu.py functionality.
    Not required for pololu.py to function on its own.

--- main.py
+++ main.py
@@ -1,22 +1,42 @@
+import json
 from machine import Pin
 from machine import PWM
 from time import sleep
 from pololu import Motor_Driver
 
-duty = 15000
+led = Pin(15, Pin.OUT)
+button = Pin(14, Pin.IN, Pin.PULL_UP)
+
+cycle = 0
+duty = 65000
 freq = 1000
 
-motor = Motor_Driver(16,17,None,None,26,27)
+motor = Motor_Driver(16,17,18,None,26,27, True)
             #(self, dir_pin, speed_pin, bk_pin, sleep_pin, motor_sense, current_sense):
+motor.speed(freq, duty)
 
 while True:
-    motor.speed(freq, duty)
+    if button.value() == True:
+        led.off()
+    else:
+        led.on()       
     if motor.pot_read() <= 400:
+        motor.speed(freq, duty)
         motor.extend()
-    if motor.pot_read() >= 65000:
-        motor.retract()
+        cycle += 1
+        print(cycle)
+    if motor.pot_read() >= 20000:
+        motor.speed(freq, duty)
+        motor.retract()     
     print(motor.pot_read())
     motor.current_read()
+    if cycle >= 2:
+        motor.brake()
+        motor.stop()
+        if button.value() == False:
+            cycle = 0
+            motor.release()
+            motor.speed(freq, duty)
     sleep(.1)
     
     
