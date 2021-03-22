4fd1c30dd85aec22da892c8ddf6e97da809a974a
A-Krau <aric.j.krause@gmail.com>
2021-03-21 17:52:07 -0700

    Update pololu.py

--- pololu.py
+++ pololu.py
@@ -4,29 +4,47 @@ from machine import ADC
 import time
 
 class Motor_Driver:
-    def __init__(self, dir_pin, speed_pin, bk_pin, sleep_pin, motor_sense, current_sense):
+    def __init__(self, dir_pin, speed_pin, bk_pin, sleep_pin, motor_pot, current_sense, indicator):
         self.dir_pin = Pin(dir_pin, Pin.OUT)
         self.pwm = PWM(Pin(speed_pin))
         if bk_pin is not None:
-            self.bk_pin = Pin(bk_pin. Pin.OUT)
+            self.bk_pin = Pin(bk_pin, Pin.OUT)
         if sleep_pin is not None:
             self.sleep_pin = Pin(sleep_pin. Pin.OUT)
             self.sleep_pin.value(1)
-        self.indicator_led = Pin(25, Pin.OUT)
-        self.motor_sense = ADC(Pin(motor_sense))
-        self.current_sense = ADC(Pin(current_sense))
+        if indicator is True:
+            self.indicator_led = Pin(25, Pin.OUT)
+        if motor_pot is not None:
+            self.motor_pot = ADC(Pin(motor_pot))
+        if motor_pot is not None:
+            self.current_sense = ADC(Pin(current_sense))
         
     def speed(self, freq, duty):
         self.pwm.freq(freq)
         self.pwm.duty_u16(duty)
         
+    def stop(self):
+        self.pwm.duty_u16(0)
+        
     def extend(self):
-        self.indicator_led.value(0)
         self.dir_pin.value(0)
+        try:
+            self.indicator_led.value(0)
+        except AttributeError:
+            pass
         
     def retract(self):
-        self.indicator_led.value(1)
         self.dir_pin.value(1)
+        try:
+            self.indicator_led.value(1)
+        except AttributeError:
+            pass
+    
+    def brake(self):
+        self.bk_pin.value(1)
+        
+    def release(self):
+        self.bk_pin.value(0)
     
     def sleep(self):
         self.sleep_pin.value(0)
@@ -35,17 +53,17 @@ class Motor_Driver:
         self.sleep_pin.value(1)
         
     def pot_read(self):
-        self.travel = ((self.motor_sense.read_u16())/93617)
+        filtered_pot = 1
+        for i in range(16):
+            filtered_pot += self.motor_pot.read_u16()
+        filtered_pot = filtered_pot / 16
+        self.travel = ((self.motor_pot.read_u16())/93617)
         self.travel = round(self.travel, 3)
-        return (self.motor_sense.read_u16())
+        return (filtered_pot)
+    
         
     def current_read(self):
         voltage = self.current_sense.read_u16()
         voltage = voltage/19858
         current = round(voltage/.5, 2)
         print("Amps: " + str(current))
-        
-    
-        
-        
-
